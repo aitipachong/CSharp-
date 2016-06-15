04953f08bf45ba5fb7d1ca4a2e58e4fc3fc925c3
aitipachong <laiqiang0010@163.com>
2016-06-15 10:23:16 +0800

    新增“aitipachong"类库的UT项目。

---
+++ UT_aitipachong/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UT_aitipachong
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
