36f51725fa61c62004c93464f67034751d2f7a9c
aitipachong <laiqiang0010@163.com>
2016-07-01 16:08:52 +0800

    获取文件的编码格式

---
+++ aitipachong/Encoding/FileEncoding.cs
@@ -0,0 +1,131 @@
+﻿// ********************************************************************
+// * 项目名称：		    aitipachong
+// * 程序集名称：	    aitipachong.Encoding
+// * 文件名称：		    FileEncoding.cs
+// * 编写者：		    Lai.Qiang
+// * 编写日期：		    2016-07-01
+// * 程序功能描述：
+// *        获取文件的编码格式
+// *
+// * 程序变更日期：
+// * 程序变更者：
+// * 变更说明：
+// * 
+// ********************************************************************
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aitipachong.Encoding
+{
+    /// <summary>
+    /// 获取文件的编码格式
+    /// </summary>
+    public class FileEncoding
+    {
+        /// <summary>
+        /// 给定文件路径，读取文件二进制数据，判断文件的编码类型
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns>文件编码类型</returns>
+        public static System.Text.Encoding GetType(string filePath)
+        {
+            
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            System.Text.Encoding r = GetType(fs);
+            fs.Close();
+            return r;
+        }
+
+        /// <summary>
+        /// 通过给定的文件流，判断文件的编码类型
+        /// </summary>
+        /// <param name="fs">文件流</param>
+        /// <returns>文件编码类型</returns>
+        public static System.Text.Encoding GetType(FileStream fs)
+        {
+            byte[] unicode = new byte[] { 0xFF, 0xFE, 0x41 };
+            byte[] UnicodeBIG = new byte[] { 0xFE, 0xFF, 0x00 };
+            byte[] UTF8 = new byte[] { 0xEF, 0xBB, 0xBF }; //带BOM
+            System.Text.Encoding reVal = System.Text.Encoding.Default;
+
+            BinaryReader r = new BinaryReader(fs, System.Text.Encoding.Default);
+            int i;
+            int.TryParse(fs.Length.ToString(), out i);
+            byte[] ss = r.ReadBytes(i);
+            if(IsUTF8Bytes(ss) || (ss[0] == 0xEF && ss[1] == 0xBB && ss[2] == 0xEF))
+            {
+                reVal = System.Text.Encoding.UTF8;
+            }
+            else if (ss[0] == 0xFE && ss[1] == 0xFF && ss[2] == 0x00)
+            {
+                reVal = System.Text.Encoding.BigEndianUnicode;
+            }
+            else if (ss[0] == 0xFF && ss[1] == 0xFE && ss[2] == 0x41)
+            {
+                reVal = System.Text.Encoding.Unicode;
+            }
+
+            r.Close();
+            return reVal;
+        }
+
+        /// <summary>
+        /// 判断是否是不带 BOM 的 UTF8 格式
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static bool IsUTF8Bytes(byte[] data)
+        {
+            int charByteCounter = 1; //计算当前正分析的字符应还有的字节数
+            byte curByte; //当前分析的字节.
+            for (int i = 0; i < data.Length; i++)
+            {
+                curByte = data[i];
+                if (charByteCounter == 1)
+                {
+                    if (curByte >= 0x80)
+                    {
+                        //判断当前
+                        while (((curByte <<= 1) & 0x80) != 0)
+                        {
+                            charByteCounter++;
+                        }
+                        //标记位首位若为非0 则至少以2个1开始 如:110XXXXX...........1111110X 
+                        if (charByteCounter == 1 || charByteCounter > 6)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    //若是UTF-8 此时第一位必须为1
+                    if ((curByte & 0xC0) != 0x80)
+                    {
+                        return false;
+                    }
+                    charByteCounter--;
+                }
+            }
+            if (charByteCounter > 1)
+            {
+                throw new Exception("非预期的byte格式");
+            }
+            return true;
+        }
+    }
+}
+
+
+/*
+
+
+
+    }
+
+}
+*/
