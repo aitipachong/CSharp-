6809d3b3d80f2cbea5b202e0b9ea7ffc55d2060f
aitipachong <laiqiang0010@163.com>
2016-07-01 16:09:32 +0800

    获取文件的编码格式

--- aitipachong/DEncrypt/RSACryption.cs
+++ aitipachong/DEncrypt/RSACryption.cs
@@ -13,11 +13,8 @@
 // * 
 // ********************************************************************
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace aitipachong.DEncrypt
 {
