09e1e94c689187fbfa7565b38918a95da8b60fb6
aitipachong <laiqiang0010@163.com>
2016-07-19 14:51:41 +0800

    Session操作类

---
+++ aitipachong/CookieSessionCache/SessionHelper.cs
@@ -0,0 +1,161 @@
+﻿// ********************************************************************
+// * 项目名称：		    aitipachong
+// * 程序集名称：	    aitipachong.CookieSessionCache
+// * 文件名称：		    SessionHelper.cs
+// * 编写者：		    Lai.Qiang
+// * 编写日期：		    2016-07-19
+// * 程序功能描述：
+// *       Session操作类
+// *
+// * 程序变更日期：
+// * 程序变更者：
+// * 变更说明：
+// * 
+// ********************************************************************
+using System.Web;
+
+namespace aitipachong.CookieSessionCache
+{
+    /// <summary>
+    /// Session操作类
+    ///     1.GetSession(string name)   根据session名获取session对象
+    ///     2.SetSession(string name, object val)   设置session
+    /// </summary>
+    public class SessionHelper
+    {
+        /// <summary>
+        /// 根据Session名，获取Session对象
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static object GetSession(string name)
+        {
+            return HttpContext.Current.Session[name];
+        }
+
+        /// <summary>
+        /// 设置Session
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public static void SetSession(string name, object value)
+        {
+            HttpContext.Current.Session.Remove(name);
+            HttpContext.Current.Session.Add(name, value);
+        }
+
+        /// <summary>
+        /// 清空所有Session
+        /// </summary>
+        public static void ClearSession()
+        {
+            HttpContext.Current.Session.Clear();
+        }
+
+        /// <summary>
+        /// 删除指定的Session
+        /// </summary>
+        /// <param name="name"></param>
+        public static void RemoveSession(string name)
+        {
+            HttpContext.Current.Session.Remove(name);
+        }
+
+        /// <summary>
+        /// 删除所有Session
+        /// </summary>
+        public static void RemoveAllSession()
+        {
+            HttpContext.Current.Session.RemoveAll();
+        }
+
+        /// <summary>
+        /// 添加Session，调动有效期为20分钟
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValue">Session值</param>
+        public static void Add(string strSessionName, string strValue)
+        {
+            HttpContext.Current.Session[strSessionName] = strValue;
+            HttpContext.Current.Session.Timeout = 20;
+        }
+
+        /// <summary>
+        /// 添加Session，调动有效期为20分钟
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValues">Session值数组</param>
+        public static void Adds(string strSessionName, string[] strValues)
+        {
+            HttpContext.Current.Session[strSessionName] = strValues;
+            HttpContext.Current.Session.Timeout = 20;
+        }
+
+        /// <summary>
+        /// 添加Session
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValue">Session值</param>
+        /// <param name="iExpires">调动有效期（分钟）</param>
+        public static void Add(string strSessionName, string strValue, int iExpires)
+        {
+            HttpContext.Current.Session[strSessionName] = strValue;
+            HttpContext.Current.Session.Timeout = iExpires;
+        }
+
+        /// <summary>
+        /// 添加Session
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValues">Session值数组</param>
+        /// <param name="iExpires">调动有效期（分钟）</param>
+        public static void Adds(string strSessionName, string[] strValues, int iExpires)
+        {
+            HttpContext.Current.Session[strSessionName] = strValues;
+            HttpContext.Current.Session.Timeout = iExpires;
+        }
+
+        /// <summary>
+        /// 读取某个Session对象值
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <returns>Session对象值</returns>
+        public static string Get(string strSessionName)
+        {
+            if (HttpContext.Current.Session[strSessionName] == null)
+            {
+                return null;
+            }
+            else
+            {
+                return HttpContext.Current.Session[strSessionName].ToString();
+            }
+        }
+
+        /// <summary>
+        /// 读取某个Session对象值数组
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <returns>Session对象值数组</returns>
+        public static string[] Gets(string strSessionName)
+        {
+            if (HttpContext.Current.Session[strSessionName] == null)
+            {
+                return null;
+            }
+            else
+            {
+                return (string[])HttpContext.Current.Session[strSessionName];
+            }
+        }
+
+        /// <summary>
+        /// 删除某个Session对象
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        public static void Del(string strSessionName)
+        {
+            HttpContext.Current.Session[strSessionName] = null;
+        }
+    }
+}
