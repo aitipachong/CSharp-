649360e1a7398791680f4aa98f08e82c55adc8a8
aitipachong <laiqiang0010@163.com>
2016-07-19 14:35:29 +0800

    缓存操作类

---
+++ aitipachong/CookieSessionCache/CacheHelper.cs
@@ -0,0 +1,100 @@
+﻿// ********************************************************************
+// * 项目名称：		    aitipachong
+// * 程序集名称：	    aitipachong.CookieSessionCache
+// * 文件名称：		    CacheHelper.cs
+// * 编写者：		    Lai.Qiang
+// * 编写日期：		    2016-07-19
+// * 程序功能描述：
+// *       缓存操作类
+// *
+// * 程序变更日期：
+// * 程序变更者：
+// * 变更说明：
+// * 
+// ********************************************************************
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace aitipachong.CookieSessionCache
+{
+    /// <summary>
+    /// 缓存操作类
+    /// </summary>
+    public class CacheHelper
+    {
+        /// <summary>
+        ///  获取数据缓存
+        /// </summary>
+        /// <param name="cacheKey">Key</param>
+        /// <returns></returns>
+        public static object GetCache(string cacheKey)
+        {
+            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
+            return objCache[cacheKey];
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="obj"></param>
+        public static void SetCache(string cacheKey, object obj)
+        {
+            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(cacheKey, obj);
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="obj"></param>
+        /// <param name="timeout"></param>
+        public static void SetCache(string cacheKey, object obj, TimeSpan timeout)
+        {
+            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(cacheKey, obj, null, DateTime.MaxValue, timeout, System.Web.Caching.CacheItemPriority.NotRemovable, null);
+        }
+
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="obj"></param>
+        /// <param name="absoluteExpiration"></param>
+        /// <param name="slidingExpiration"></param>
+        public static void SetCache(string cacheKey, object obj, DateTime absoluteExpiration, TimeSpan slidingExpiration)
+        {
+            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(cacheKey, obj, null, absoluteExpiration, slidingExpiration);
+        }
+
+        /// <summary>
+        /// 移除指定数据缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        public static void RemoveCache(string cacheKey)
+        {
+            System.Web.Caching.Cache cache = HttpRuntime.Cache;
+            cache.Remove(cacheKey);
+        }
+
+        /// <summary>
+        /// 移除全部缓存
+        /// </summary>
+        public static void RemoveAllCache()
+        {
+            System.Web.Caching.Cache cache = HttpRuntime.Cache;
+            IDictionaryEnumerator cacheEnum = cache.GetEnumerator();
+            while(cacheEnum.MoveNext())
+            {
+                cache.Remove(cacheEnum.Key.ToString());
+            }
+        }
+    }
+}
