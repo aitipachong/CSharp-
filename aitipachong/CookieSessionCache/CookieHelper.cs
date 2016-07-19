0d0a40a4e6c9f870c0adbbfafc62e25e1b4f9251
aitipachong <laiqiang0010@163.com>
2016-07-19 14:42:47 +0800

    Cookie操作类

---
+++ aitipachong/CookieSessionCache/CookieHelper.cs
@@ -0,0 +1,85 @@
+﻿// ********************************************************************
+// * 项目名称：		    aitipachong
+// * 程序集名称：	    aitipachong.CookieSessionCache
+// * 文件名称：		    CookieHelper.cs
+// * 编写者：		    Lai.Qiang
+// * 编写日期：		    2016-07-19
+// * 程序功能描述：
+// *       Cookie操作类
+// *
+// * 程序变更日期：
+// * 程序变更者：
+// * 变更说明：
+// * 
+// ********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace aitipachong.CookieSessionCache
+{
+    /// <summary>
+    /// Cookie操作类
+    /// </summary>
+    public class CookieHelper
+    {
+        /// <summary>
+        /// 清除指定Cookie
+        /// </summary>
+        /// <param name="cookieName"></param>
+        public static void ClearCookie(string cookieName)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[cookieName];
+            if(cookie != null)
+            {
+                cookie.Expires = DateTime.Now.AddYears(-3);
+                HttpContext.Current.Response.Cookies.Add(cookie);
+            }
+        }
+
+        /// <summary>
+        /// 获取指定Cookie值
+        /// </summary>
+        /// <param name="cookieName"></param>
+        /// <returns></returns>
+        public static string GetCookieValue(string cookieName)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies[cookieName];
+            string str = string.Empty;
+            if(cookie != null)
+            {
+                str = cookie.Value;
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 添加一个Cookie(24小时过期)
+        /// </summary>
+        /// <param name="cookieName"></param>
+        /// <param name="cookieValue"></param>
+        public static void SetCookie(string cookieName, string cookieValue)
+        {
+            SetCookie(cookieName, cookieValue, DateTime.Now.AddDays(1.0));
+        }
+
+        /// <summary>
+        /// 添加一个Cookie
+        /// </summary>
+        /// <param name="cookieName"></param>
+        /// <param name="cookieValue"></param>
+        /// <param name="expires"></param>
+        public static void SetCookie(string cookieName, string cookieValue, DateTime expires)
+        {
+            HttpCookie cookie = new HttpCookie(cookieName)
+            {
+                Value = cookieValue,
+                Expires = expires
+            };
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+    }
+}
