aa40f447a8e1016bf88c8cafb41fe2dea37928c3
aitipachong <laiqiang0010@163.com>
2016-08-19 16:53:00 +0800

    剔除了无用的“using"

--- aitipachong/Database/DbUtility.cs
+++ aitipachong/Database/DbUtility.cs
@@ -17,9 +17,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace aitipachong.Database
 {
