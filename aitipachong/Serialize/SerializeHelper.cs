1f9a4959d0d48f5675f1e2241c2d2b6eaa2620d2
aitipachong <laiqiang0010@163.com>
2016-08-30 15:29:47 +0800

    增加“序列化操作类”包括：XML序列化/反序列化、JSON序列化/反序列化、SoapFormatter序列化/反序列化、BinaryFormatter序列化/反序列化

---
+++ aitipachong/Serialize/SerializeHelper.cs
@@ -0,0 +1,242 @@
+﻿// ********************************************************************
+// * 项目名称：		    aitipachong
+// * 程序集名称：	    aitipachong.Serialize
+// * 文件名称：		    SerializeHelper.cs
+// * 编写者：		    Lai.Qiang
+// * 编写日期：		    2016-08-30
+// * 程序功能描述：
+// *        序列化操作类
+// *            1.XML序列化/反序列化
+// *            2.JSON序列化/反序列化
+// *            3.SoapFormatter序列化/反序列化
+// *            4.BinaryFormatter序列化/反序列化
+// *
+// * 程序变更日期：
+// * 程序变更者：
+// * 变更说明：
+// * 
+// ********************************************************************
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace aitipachong.Serialize
+{
+    /// <summary>
+    /// 序列化操作类
+    /// </summary>
+    public class SerializeHelper
+    {
+        public SerializeHelper()
+        { }
+
+        #region XML序列化
+        /// <summary>
+        /// 对象序列化为XML文件
+        /// </summary>
+        /// <param name="obj">待序列化对象</param>
+        /// <param name="fileName">序列化为XML文件的保存路径</param>
+        public static void Save(object obj, string fileName)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                XmlSerializer serializer = new XmlSerializer(obj.GetType());
+                serializer.Serialize(fs, obj);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+
+        /// <summary>
+        /// 把XML文件反序列化为对象
+        /// </summary>
+        /// <param name="type">对象类型</param>
+        /// <param name="fileName">文件路径</param>
+        /// <returns></returns>
+        public static object Load(Type type, string fileName)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                XmlSerializer serializer = new XmlSerializer(type);
+                return serializer.Deserialize(fs);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+
+        /// <summary>
+        /// 文本化XML序列化
+        /// </summary>
+        /// <typeparam name="T">泛型：对象类型</typeparam>
+        /// <param name="item">对象</param>
+        /// <returns></returns>
+        public string ToXml<T>(T item)
+        {
+            XmlSerializer serializer = new XmlSerializer(item.GetType());
+            StringBuilder sb = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(sb))
+            {
+                serializer.Serialize(writer, item);
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 文本化XML反序列化
+        /// </summary>
+        /// <typeparam name="T"> </typeparam>
+        /// <param name="str">字符串序列</param>
+        /// <returns></returns>
+        public T FromXml<T>(string str)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (XmlReader reader = new XmlTextReader(new StringReader(str)))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+        #endregion
+
+        #region JSON序列化
+        /// <summary>
+        /// JsonSerializer序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public string ToJson<T>(T item)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(item.GetType());
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, item);
+                return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// JsonSerializer反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public T FromJson<T>(string str) where T : class
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            using (MemoryStream ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(str)))
+            {
+                return serializer.ReadObject(ms) as T;
+            }
+        }
+        #endregion
+
+        #region SoapFormatter序列化
+        /// <summary>
+        /// SoapFormatter序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public string ToSoap<T>(T item)
+        {
+            SoapFormatter formatter = new SoapFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                formatter.Serialize(ms, item);
+                ms.Position = 0;
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(ms);
+                return xmlDoc.InnerXml;
+            }
+        }
+
+        /// <summary>
+        /// SoapFormatter反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public T FromSoap<T>(string str)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(str);
+            SoapFormatter formatter = new SoapFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                xmlDoc.Save(ms);
+                ms.Position = 0;
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+        #endregion
+
+        #region BinaryFormatter序列化
+
+        /// <summary>
+        /// BinaryFormatter序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public string ToBinary<T>(T item)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                formatter.Serialize(ms, item);
+                ms.Position = 0;
+                byte[] bytes = ms.ToArray();
+                StringBuilder sb = new StringBuilder();
+                foreach(byte bt in bytes)
+                {
+                    sb.Append(string.Format("{0:X2}", bt));
+                }
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// BinaryFormatter反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public T FromBinary<T>(string str)
+        {
+            int intLen = str.Length / 2;
+            byte[] bytes = new byte[intLen];
+            for(int i = 0; i < intLen; i++)
+            {
+                int ibyte = Convert.ToInt32(str.Substring(i * 2, 2), 16);
+                bytes[i] = (byte)ibyte;
+            }
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+        #endregion
+    }
+}
