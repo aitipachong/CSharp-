245f5341fb63def89f9662695e159413c9c07797
aitipachong <laiqiang0010@163.com>
2016-10-17 14:44:11 +0800

    增加了拼音转换类

---
+++ UT_aitipachong/PinYin/UT_PinYinHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using aitipachong.PinYin;
+
+namespace UT_aitipachong.PinYin
+{
+    [TestClass]
+    public class UT_PinYinHelper
+    {
+        [TestMethod]
+        public void UT_GetFirstLetter_V1()
+        {
+            string hz = "赖强";
+            try
+            {
+                string firstLetter = PinYinHelper.GetFirstLetter(hz);
+                Assert.AreEqual("LQ", firstLetter);
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void UT_GetCodstring_V1()
+        {
+            string hz = "赖强";
+            try
+            {
+                string firstLetter = PinYinHelper.GetCodstring(hz);
+                Assert.AreEqual("LQ", firstLetter);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void UT_ConvertPinYin_V1()
+        {
+            string hz = "赖强";
+            try
+            {
+                PinYinHelper helper = new PinYinHelper();
+                string pinyin = helper.ConvertPinYin(hz);
+                Assert.AreEqual("laiqiang", pinyin.Trim().ToLower());
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+    }
+}
